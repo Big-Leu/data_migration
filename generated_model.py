0f8d888041e2408566c43eb601d808335be8ac20
Big-Leu <100833208+Big-Leu@users.noreply.github.com>
2024-05-16 15:16:30 +0530

    data-baackup

---
+++ generated_model.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class MyModel(Base):
+    __tablename__ = 'mymodel'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    
+    contact_number = Column(String)
+    
+    name = Column(String)
+    
+    age = Column(Integer)
+    
+    address = Column(String)
+    
